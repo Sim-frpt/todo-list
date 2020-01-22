869a1dcaa1b0406c2943e577bee185c1981281f2
Simon Fraipont <simon.fraipont@gmail.com>
2020-01-22 18:05:38 +0100

    refactor html and put modules in module folder

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import "normalize.css";
 import "./assets/styles/style.css";
-import { taskFactory } from "./task";
-import { generateTaskForm } from "./dom-manipulation";
+import { taskFactory } from "./modules/task";
+import { generateTaskForm } from "./modules/dom-manipulation";
 
 console.log( taskFactory( { title:'hi', description:'lol', dueDate:'test', priority:'ier', notes:'xcv', project:'woie' }));
 
 const form = generateTaskForm();
-document.body.append( form );
-//console.log( generateTaskForm() );
+//document.body.append( form );
