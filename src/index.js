571dabd5265391876fb1007488d9ef104d467cbd
Simon Fraipont <simon.fraipont@gmail.com>
2020-01-23 12:35:42 +0100

    add proper forms for projects and tasks, remove dynamic js form

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import "normalize.css";
 import "./assets/styles/style.css";
 import { taskFactory } from "./modules/task";
-import { generateTaskForm } from "./modules/dom-manipulation";
+import { revealProjectInput, removeProjectInput } from "./modules/dom-manipulation";
 
-console.log( taskFactory( { title:'hi', description:'lol', dueDate:'test', priority:'ier', notes:'xcv', project:'woie' }));
+//console.log( taskFactory( { title:'hi', description:'lol', dueDate:'test', priority:'ier', notes:'xcv', project:'woie' }));
 
-const form = generateTaskForm();
-//document.body.append( form );
+const revealProjectFormButton = document.getElementsByClassName( "reveal__project-input" )[0];
+const hideProjectFormButton = document.getElementsByClassName( "button__close-project" )[0];
+
+revealProjectFormButton.addEventListener( 'click', revealProjectInput );
+hideProjectFormButton.addEventListener( 'click', removeProjectInput );
--- src/modules/dom-manipulation.js
+++ src/modules/dom-manipulation.js
@@ -1,97 +1,14 @@
+const addProjectDiv = document.getElementsByClassName( 'add__project' )[0];
 
-const createLabel = ( inputName, text ) => {
-  const label = document.createElement( 'label' );
-  label.setAttribute( 'for', inputName );
-  label.textContent = text;
-
-  return label;
-};
-
-const createInput = ( type, id ) => {
-  const input = document.createElement( 'input' );
-  input.type = type;
-  input.setAttribute( 'id', id);
-  input.setAttribute( 'name', id);
-
-  return input;
-};
-
-const createSpecialInput = ( elementType, name ) => {
-  const element = document.createElement( elementType );
-  element.setAttribute( 'id', name );
-  element.setAttribute( 'name', name );
-
-  return element;
-};
-
-const appendOptionsToSelect = ( options, selectElement ) => {
-  if ( ! options.length ) {
-    return;
-  }
-
-  options.forEach( option => {
-    const optionNode = document.createElement( 'option' );
-
-    optionNode.value = option;
-    optionNode.textContent = option.toUpperCase();
-
-    selectElement.append( optionNode );
-  });
+const revealProjectInput = ( event ) => {
+  addProjectDiv.style.display = 'block';
 }
 
-function generateTaskForm() {
-  const form = document.createElement( 'form' );
-  form.classList.add( "task__form" );
+const removeProjectInput = ( event ) => {
+  const addProjectInput = document.getElementsByClassName( "project__input" )[0];
 
-  const taskTitle = createLabel( 'title', 'Title' );
-  const title = createInput( 'text', 'title' );
-
-  const taskDescription = createLabel( 'description', 'Title' );
-  const description = createSpecialInput( 'textarea', 'description' );
-
-  const taskDueDate = createLabel( 'due-date', 'Deadline' );
-  const dueDate = createInput( 'date', 'due-date' );
-
-  const taskPriority = createLabel( 'priority', 'Priority' );
-  const priority = createSpecialInput ( 'select', 'priority' );
-
-  const priorityOptions = [ 'low', 'normal', 'high' ];
-  appendOptionsToSelect( priorityOptions, priority );
-
-  const taskNotes = createLabel( 'notes', 'Notes' );
-  const notes = createSpecialInput( 'textarea', 'notes' );
-
-  const taskProject = createLabel( 'project', 'Project' );
-  const project = createSpecialInput( 'select', 'project' );
-
-  appendOptionsToSelect( [ 'default' ], project );
-
-  const closeButton = document.createElement( 'button' );
-  closeButton.classList.add( 'button__close' );
-  closeButton.textContent = 'Close';
-
-  const submitButton = document.createElement( 'button' );
-  submitButton.classList.add( 'button__submit' );
-  submitButton.textContent = 'Save';
-
-  form.append(
-    taskTitle,
-    title,
-    taskDescription,
-    description,
-    taskDueDate,
-    dueDate,
-    taskPriority,
-    priority,
-    taskNotes,
-    notes,
-    taskProject,
-    project,
-    closeButton,
-    submitButton,
-  );
-
-  return form;
+  addProjectInput.textContent = '';
+  addProjectDiv.style.display = '';
 }
 
-export { generateTaskForm };
+export { revealProjectInput, removeProjectInput };
